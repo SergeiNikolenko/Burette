f763223e7e70360590c85ead555f6d6ee2357959
Nikolenko.Sergei <Nikolenko.Sergei@icloud.com>
2026-06-23 01:14:28 +0300

    Support NetCDF trajectory drops

--- PreviewExtension/Web/viewer.js
+++ PreviewExtension/Web/viewer.js
@@ -84,7 +84,7 @@
     customConfigPath: null,
     extraArguments: null
   };
-  const DOCKING_COORDINATE_TRAJECTORY_FORMATS = new Set(['xtc', 'trr', 'dcd', 'nctraj', 'lammpstrj']);
+  const DOCKING_COORDINATE_TRAJECTORY_FORMATS = new Set(['xtc', 'trr', 'dcd', 'nctraj', 'nc', 'ncdf', 'netcdf', 'ncrst', 'lammpstrj']);
   const DOCKING_MODEL_TRAJECTORY_FORMATS = new Set(['pdb', 'pdbqt', 'mmcif', 'gro']);
   const DOCKING_TOPOLOGY_TRAJECTORY_FORMATS = new Set(['top', 'psf', 'prmtop']);
   const STRUCTURE_DRAG_MIME = 'application/x-burrete-structure-paths';
