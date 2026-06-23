710fcd528c201204b79e3e74141f10fdc7c049f4
Nikolenko.Sergei <Nikolenko.Sergei@icloud.com>
2026-06-23 20:00:02 +0300

    Remove xyzrender apply settings context action

--- PreviewExtension/Web/viewer.js
+++ PreviewExtension/Web/viewer.js
@@ -5404,13 +5404,6 @@
         hideSelectedXyzrenderElements();
         hideXyzrenderSheetContextMenu();
       });
-      appendXyzrenderMenuButton(actions, 'Apply Current Settings', () => {
-        const toolbar = document.getElementById('buret-toolbar');
-        if (!toolbar) throw new Error('xyzrender settings toolbar is unavailable.');
-        pushXyzrenderActionHistory(item, 'apply settings');
-        applyXyzrenderSelectionControls(readXyzrenderControlsForm(toolbar));
-        hideXyzrenderSheetContextMenu();
-      });
       appendXyzrenderMenuButton(actions, 'Dim Others', () => {
         pushXyzrenderActionHistory(item, 'dim others');
         dimUnselectedXyzrenderElements(item);
