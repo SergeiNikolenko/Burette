c39e21fe0efe4572cf5dacaf209ac06bb2e19044
Nikolenko.Sergei <Nikolenko.Sergei@icloud.com>
2026-07-14 01:08:50 +0300

    Add DataWarrior structure file support

---
+++ scripts/openchemlib-global.ts
@@ -0,0 +1,8 @@
+import OpenChemLib from "openchemlib";
+
+Object.defineProperty(globalThis, "BurreteOpenChemLib", {
+  configurable: false,
+  enumerable: false,
+  value: OpenChemLib,
+  writable: false,
+});
