3b6b29f9717501ae4a876119157497524cf89d20
Nikolenko.Sergei <Nikolenko.Sergei@icloud.com>
2026-07-17 00:26:37 +0300

    Import the complete native PM6 parameter domain

--- crates/burrete-compute-core/src/lib.rs
+++ crates/burrete-compute-core/src/lib.rs
@@ -64,15 +64,15 @@ pub use mmff_extract::{decode_native_mmff_parameters, MmffExtractError, NativeMm
 pub use semiempirical::{
     contract_rm1_pair_fock, evaluate_rm1, evaluate_rm1_with_accelerators,
     evaluate_rm1_with_pair_contractor, evaluate_rm1_with_prepared_pairs_and_accelerators,
-    evaluate_semiempirical, pm6_d3_dispersion_energy, pm6_h4_covalent_radius, pm6_h4_energy,
-    pm6_hh_repulsion_energy, rm1_fock_pairs, rm1_multipole_parameters,
+    evaluate_semiempirical, pm6_d3_dispersion_energy, pm6_full_parameters, pm6_h4_covalent_radius,
+    pm6_h4_energy, pm6_hh_repulsion_energy, rm1_fock_pairs, rm1_multipole_parameters,
     rm1_nuclear_repulsion_energy, rm1_parameters, rm1_rotated_pair_integrals, rm1_sp_overlap,
     rm1_two_center_integrals, semiempirical_nuclear_repulsion_energy, semiempirical_parameters,
     solve_closed_shell_scf, solve_closed_shell_scf_with_eigensolver, symmetric_eigendecomposition,
-    Rm1Evaluation, Rm1FockPair, Rm1MultipoleParameters, Rm1OverlapMatrix, Rm1RotatedPairIntegrals,
-    Rm1TwoCenterIntegrals, SemiempiricalAtom, SemiempiricalElementParameters, SemiempiricalError,
-    SemiempiricalMethod, SemiempiricalMolecule, SemiempiricalScfOptions, SemiempiricalScfResult,
-    SemiempiricalScfStatus,
+    Pm6FullElementParameters, Rm1Evaluation, Rm1FockPair, Rm1MultipoleParameters, Rm1OverlapMatrix,
+    Rm1RotatedPairIntegrals, Rm1TwoCenterIntegrals, SemiempiricalAtom,
+    SemiempiricalElementParameters, SemiempiricalError, SemiempiricalMethod, SemiempiricalMolecule,
+    SemiempiricalScfOptions, SemiempiricalScfResult, SemiempiricalScfStatus,
 };
 
 use std::{cmp::Ordering, fmt, mem::size_of, num::NonZeroUsize};
