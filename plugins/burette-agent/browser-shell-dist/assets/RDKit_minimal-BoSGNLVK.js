1caaaebc3b93813c034eb96b845d2b0e98712c9e
Nikolenko.Sergei <Nikolenko.Sergei@icloud.com>
2026-08-20 20:35:38 +0200

    Regenerate grid-ui bundle and agent shell dist

---
+++ plugins/burette-agent/browser-shell-dist/assets/RDKit_minimal-BoSGNLVK.js
@@ -0,0 +1,1 @@
+var e=``+new URL(`RDKit_minimal-tnscgqxm.wasm`,import.meta.url).href;export{e as default};
---
+++ plugins/burette-agent/browser-shell-dist/assets/resources-DJEzgTo-.js
@@ -0,0 +1,1 @@
+var e=``+new URL(`resources-D5ymtJwZ.json`,import.meta.url).href;export{e as default};
