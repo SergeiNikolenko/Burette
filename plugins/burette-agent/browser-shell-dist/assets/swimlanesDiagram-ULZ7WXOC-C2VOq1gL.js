68828a3d09ef282d043a30250babf740bb0cc5f1
Nikolenko.Sergei <Nikolenko.Sergei@icloud.com>
2026-09-21 03:15:24 +0300

    Regenerate browser plugin for Chemical Space updates

--- plugins/burette-agent/browser-shell-dist/assets/swimlanesDiagram-ULZ7WXOC-C2VOq1gL.js
+++
@@ -1,8 +0,0 @@
-import"./chunk-NSK5VX7P-D_vZ_Lon.js";import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./chunk-X3CZISLH-C5MQD9l6.js";import"./chunk-I66GZJ75-BTMQ2IrL.js";import"./chunk-4I5QYGJK-Cal_9A96.js";import"./chunk-WRU74C26-CKBvXYIE.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./chunk-UBXNYLIW-DRkdnqzm.js";import"./chunk-2GRJ4B5K-BlO2EQDH.js";import"./chunk-XXDRQBXY-DxXD-La7.js";import"./chunk-KBJHAD2P-D0lTGM1W.js";import"./chunk-W5SLKNZC-DBpThLN1.js";import"./chunk-QR6OTTB3-DzpGRhxL.js";import"./chunk-7Z6QIM7H-CEMra15j.js";import"./chunk-J7OUQ5F2-TxXWnZto.js";import"./chunk-ZIRB5QZD-C6fEPe3t.js";import{r as t,t as n}from"./chunk-JQJVKLGR-BII8Oo9U.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
-  .swimlane.cluster rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
